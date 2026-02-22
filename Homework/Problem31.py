75101fded329a8b0d9942378e22e3df89a9a45ca
Alen2904 <alensoy2904@gmail.com>
2026-02-22 15:48:55 +0530

    completed

---
+++ Homework/Problem31.py
@@ -0,0 +1,18 @@
+class Solution:
+    def searchInsert(self, nums, target):
+        if not nums:
+            return 0
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return left
+
+print(Solution().searchInsert([1,3,5,6], 5))
+print(Solution().searchInsert([1,3,5,6], 2))
+print(Solution().searchInsert([1,3,5,6], 7))
